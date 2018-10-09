7e7bda0eaad5d75d5ce8212ccced8bf143f322b8
caobo001 <cao_fg@163.com>
2018-10-09 22:30:19 +0800

    可视餐厅多视频滑动

--- mgj_food/goods/shop/shop.js
+++ mgj_food/goods/shop/shop.js
@@ -221,7 +221,7 @@ Page(Object.assign({}, merchantShop,{
 	touchstartCallback: function(e) {
         // 单手指缩放开始，也不做任何处理 
         if(e.touches.length == 1) return;
-        // 注意touchstartCallback 真正代码的开始 // 一开始我并没有这个回调函数，会出现缩小的时候有瞬间被放大过程的bug // 当两根手指放上去的时候，就将distance 初始化。 
+        // 一开始我并没有这个回调函数，会出现缩小的时候有瞬间被放大过程的bug // 当两根手指放上去的时候，就将distance 初始化。 
         let xMove = e.touches[1].clientX - e.touches[0].clientX;
         let yMove = e.touches[1].clientY - e.touches[0].clientY;
         let distance = Math.sqrt(xMove * xMove + yMove * yMove);
